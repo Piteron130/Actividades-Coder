f69834f34f0b566e6b61ec85253b4210eb04a3ce
Piteron130 <58676428+Piteron130@users.noreply.github.com>
2023-10-02 10:06:52 -0300

    Add files via upload

---
+++ p-actividad.js
@@ -0,0 +1,40 @@
+// let numero = Number(prompt(`Bienvenido a la Calculadora 
+//     1 Multiplicar
+//     2 Dividir    
+//     3 Sumar    
+//     4 Restar`))
+// if (numero == 1) {
+//     let valor1 = Number(prompt("Ingresa tú primer valor, que lo multiplicaré"))
+//     let valor2 = Number(prompt("Ingresa tú segundo valor, que lo multiplicaré"))
+//     let resultado = valor1 * valor2
+//     console.log(`La multiplicación de ${valor1}X${valor2} es: ${resultado}`)
+// } else if (numero == 2) {
+//     let valor1 = Number(prompt("Ingresa tú primer valor, que lo dividiré"))
+//     let valor2 = Number(prompt("Ingresa tú segundo valor, que lo dividiré"))
+//     let resultado = valor1 / valor2
+//     console.log(`La división de ${valor1}/${valor2} es: ${resultado}`)
+// } else if (numero == 3) {
+//     let valor1 = Number(prompt("Ingresa tú primer valor, que lo sumaré"))
+//     let valor2 = Number(prompt("Ingresa tú segundo valor, que lo sumaré"))
+//     let resultado = valor1 + valor2
+//     console.log(`La suma de ${valor1}+${valor2} es: ${resultado}`)
+// } else if (numero == 4) {
+//     let valor1 = Number(prompt("Ingresa tú primer valor, que lo restaré"))
+//     let valor2 = Number(prompt("Ingresa tú segundo valor, que lo restaré"))
+//     let resultado = valor1 - valor2
+//     console.log(`La resta de ${valor1}-${valor2} es: ${resultado}`)
+// } else {
+//     console.log("No ingresaste ningun valor, correspondiente")
+// }
+
+
+
+
+// let Ingreso = Number(prompt("Dime un número, te los mostraré todos"))
+// if (Ingreso <= 100) {
+//     for (let i = 1; i <= Ingreso; i++) {
+//         console.log(`El número solicitado es: ${Ingreso}, Aquí están: ${i}`)
+//     }
+// } else {
+//     alerta = alert("El número no debe ser mayor a 100")
+// }
